71aa3fbd95daa0b17aa9a084234e27ce9599be2b
Rogério Luiz Mazzotti <rogeriomazzotti@hotmail.com>
2019-02-10 20:23:31 -0200

    End of Section 8

---
+++ DatingApp.Api/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,36 @@
+using System.Linq;
+using AutoMapper;
+using DatingApp.Api.Dtos;
+using DatingApp.Api.Models;
+
+namespace DatingApp.Api.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserForListDto>()
+            .ForMember(r => r.PhotoUrl, opt =>
+            {
+                opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+            })
+            .ForMember(r => r.Age, opt =>
+            {
+                opt.ResolveUsing(d => d.DateOfBIrth.CalculateAge());
+            });
+
+            CreateMap<User, UserForDetailedDto>()
+            .ForMember(r => r.PhotoUrl, opt =>
+            {
+                opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+            })
+            .ForMember(r => r.Age, opt =>
+            {
+                opt.ResolveUsing(d => d.DateOfBIrth.CalculateAge());
+            });
+
+
+            CreateMap<Photo, PhotosForDetailedDto>();
+        }
+    }
+}
--- DatingApp.Api/Startup.cs
+++ DatingApp.Api/Startup.cs
@@ -19,6 +19,7 @@ using System.Net;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using DatingApp.Api.Helpers;
+using AutoMapper;
 
 namespace DattingApp.Api
 {
@@ -35,9 +36,20 @@ namespace DattingApp.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+            .AddJsonOptions(opt =>
+            {
+                opt.SerializerSettings.ReferenceLoopHandling =
+                Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
             services.AddCors();
+
+            services.AddAutoMapper();
+            services.AddTransient<Seed>();
+
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IDatingRepository, DatingRepository>();
+
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(Options =>
             {
@@ -53,7 +65,7 @@ namespace DattingApp.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
         {
             if (env.IsDevelopment())
             {
@@ -67,7 +79,8 @@ namespace DattingApp.Api
                     {
                         context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
                         var error = context.Features.Get<IExceptionHandlerFeature>();
-                        if(error!=null){
+                        if (error != null)
+                        {
                             context.Response.AddApplicationError(error.Error.Message);
                             await context.Response.WriteAsync(error.Error.Message);
                         }
@@ -77,6 +90,7 @@ namespace DattingApp.Api
             }
 
             // app.UseHttpsRedirection();
+            //seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseMvc();
