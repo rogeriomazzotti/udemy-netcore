bd899aa0a288231b25612140ea224d7784de198c
Rogério Luiz Mazzotti <rogeriomazzotti@hotmail.com>
2019-02-24 14:41:43 -0300

    End of section 12

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { PhotoEditorComponent } from "./members/photo-editor/photo-editor.component";
 import { MemberEditComponent } from "./members/member-edit/member-edit.component";
 import { MemberListResolver } from "./_resolvers/member-list.resolver";
 import { MemberDetailComponent } from "./members/member-detail/member-detail.component";
@@ -9,7 +9,7 @@ import { AlertifyService } from "./_services/alertify.service";
 import { AuthService } from "./_services/auth.service";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { NgxGalleryModule } from "ngx-gallery";
 import { AppComponent } from "./app.component";
@@ -18,7 +18,7 @@ import { NavComponent } from "./nav/nav.component";
 import { HomeComponent } from "./home/home.component";
 import { RegisterComponent } from "./register/register.component";
 import { ErrorInterceptorProvider } from "./_services/error.interceptor";
-import { BsDropdownModule, TabsModule } from "ngx-bootstrap";
+import { BsDropdownModule, TabsModule, BsDatepickerModule } from "ngx-bootstrap";
 import { MemberListComponent } from "./members/member-list/member-list.component";
 import { ListsComponent } from "./lists/lists.component";
 import { MessagesComponent } from "./messages/messages.component";
@@ -27,7 +27,7 @@ import { JwtModule } from "@auth0/angular-jwt";
 import { MemberDetailResolver } from "./_resolvers/member-detail.resolver";
 import { MemberEditResolver } from "./_resolvers/member-edit.resolver";
 import { PreventUnsavedChanges } from "./_guards/prevent-unsaved-changes.guard";
-import { FileUploadModule } from 'ng2-file-upload';
+import { FileUploadModule } from "ng2-file-upload";
 
 export function tokenGetter() {
   return localStorage.getItem("token");
@@ -51,8 +51,9 @@ export function tokenGetter() {
     BrowserModule,
     HttpClientModule,
     FormsModule,
-
+    ReactiveFormsModule,
     BsDropdownModule.forRoot(),
+    BsDatepickerModule.forRoot(),
     RouterModule.forRoot(appRoutes),
     NgxGalleryModule,
     TabsModule.forRoot(),
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,10 @@
-import { AlertifyService } from './../_services/alertify.service';
+import { Router } from "@angular/router";
+import { AlertifyService } from "./../_services/alertify.service";
 import { AuthService } from "./../_services/auth.service";
-import { Component, OnInit,  Output, EventEmitter } from "@angular/core";
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { FormGroup, Validators, FormBuilder } from "@angular/forms";
+import { BsDatepickerConfig } from "ngx-bootstrap";
+import { User } from "../_models/user";
 
 @Component({
   selector: "app-register",
@@ -8,22 +12,73 @@ import { Component, OnInit,  Output, EventEmitter } from "@angular/core";
   styleUrls: ["./register.component.css"]
 })
 export class RegisterComponent implements OnInit {
-  model: any = {};
+  user: User;
   @Output() cancelRegister = new EventEmitter();
+  registerForm: FormGroup;
 
-  constructor(private authService: AuthService, private alertify:AlertifyService) {}
+  bsConfig: Partial<BsDatepickerConfig>;
 
-  ngOnInit() {}
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.bsConfig = {
+      containerClass: "theme-red"
+    };
+    this.createRegisterForm();
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.fb.group(
+      {
+        gender: ["male"],
+        username: ["", Validators.required],
+        knowAs: ["", Validators.required],
+        dateOfBirth: ["", Validators.required],
+        city: ["", Validators.required],
+        country: ["", Validators.required],
+        password: [
+          "",
+          [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(8)
+          ]
+        ],
+        confirmPassword: ["", Validators.required]
+      },
+      { validator: this.passwordMatchValidator }
+    );
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get("password").value === g.get("confirmPassword").value
+      ? null
+      : { mismatch: true };
+  }
 
   register() {
-    this.authService.register(this.model).subscribe(
-      () => {
-        this.alertify.sucess("registration successful");
-      },
-      error => {
-        this.alertify.error(error);
-      }
-    );
+    if (this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+
+      this.authService.register(this.user).subscribe(
+        () => {
+          this.alertify.sucess("Registration successful");
+        },
+        error => {
+          this.alertify.error(error);
+        },
+        () => {
+          this.authService.login(this.user).subscribe(() => {
+            this.router.navigate(["/members"]);
+          });
+        }
+      );
+    }
   }
 
   cancel() {
