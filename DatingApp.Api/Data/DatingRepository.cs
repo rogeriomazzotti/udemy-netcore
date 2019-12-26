d1f3f28b7348576804dc645696fd744056787f75
Rogério Luiz Mazzotti <rogerio.mazzotti@architettura.com.br>
2019-12-26 12:35:15 -0300

    Lazy Loading

--- DatingApp.Api/Data/DatingRepository.cs
+++ DatingApp.Api/Data/DatingRepository.cs
@@ -36,12 +36,12 @@ namespace DatingApp.Api.Data {
         }
 
         public async Task<User> GetUser (int id) {
-            var user = await _context.Users.Include (p => p.Photos).FirstOrDefaultAsync (u => u.Id == id);
+            var user = await _context.Users.FirstOrDefaultAsync (u => u.Id == id);
             return user;
         }
 
         public async Task<PagedList<User>> GetUsers (UserParams userParams) {
-            var users = _context.Users.Include (p => p.Photos).AsQueryable ();
+            var users = _context.Users.OrderByDescending(u=> u.LastActive).AsQueryable ();
             users = users.Where (u => u.Id != userParams.UserId);
             users = users.Where (u => u.Gender == userParams.Gender);
 
@@ -76,7 +76,7 @@ namespace DatingApp.Api.Data {
         }
 
         private async Task<IEnumerable<int>> GetUserLikes (int id, bool likers) {
-            var user = await _context.Users.Include (x => x.Likers).Include (x => x.Likees)
+            var user = await _context.Users
                 .FirstOrDefaultAsync (u => u.Id == id);
 
             if (likers) {
@@ -95,9 +95,7 @@ namespace DatingApp.Api.Data {
 
         public async Task<PagedList<Message>> GetMessageForUser (MessageParams messageParams) {
             var messages = _context.Messages
-                .Include (u => u.Sender).ThenInclude (p => p.Photos)
-                .Include (u => u.Recipient).ThenInclude (p => p.Photos)
-                .AsQueryable ();
+                               .AsQueryable ();
 
             switch (messageParams.MessageContainer) {
                 case "Inbox":
@@ -117,8 +115,6 @@ namespace DatingApp.Api.Data {
 
         public async Task<IEnumerable<Message>> GetMessageThread (int userId, int recipientId) {
             var messages = await _context.Messages
-                .Include (u => u.Sender).ThenInclude (p => p.Photos)
-                .Include (u => u.Recipient).ThenInclude (p => p.Photos)
                 .Where (m => m.RecipientId == userId && m.RecipientDeleted == false && m.SenderId == recipientId ||
                     m.RecipientId == recipientId && m.SenderDeleted == false && m.SenderId == userId)
 
--- DatingApp.Api/Startup.cs
+++ DatingApp.Api/Startup.cs
@@ -1,102 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.Api.Data;
+using DatingApp.Api.Helpers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using System.Net;
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Http;
-using DatingApp.Api.Helpers;
-using AutoMapper;
 
-namespace DattingApp.Api
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace DattingApp.Api {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
-            .AddJsonOptions(opt =>
-            {
-                opt.SerializerSettings.ReferenceLoopHandling =
-                Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddDbContext<DataContext> (x => {
+                x.UseLazyLoadingProxies();
+                x.UseSqlite (Configuration.GetConnectionString ("DefaultConnection"));
             });
-            services.AddCors();
+            services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_1)
+                .AddJsonOptions (opt => {
+                    opt.SerializerSettings.ReferenceLoopHandling =
+                        Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
+            services.AddCors ();
 
-            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
-            services.AddAutoMapper();
-            services.AddTransient<Seed>();
+            services.Configure<CloudinarySettings> (Configuration.GetSection ("CloudinarySettings"));
+            services.AddAutoMapper ();
+            services.AddTransient<Seed> ();
 
-            services.AddScoped<IAuthRepository, AuthRepository>();
-            services.AddScoped<IDatingRepository, DatingRepository>();
+            services.AddScoped<IAuthRepository, AuthRepository> ();
+            services.AddScoped<IDatingRepository, DatingRepository> ();
 
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(Options =>
-            {
-                Options.TokenValidationParameters = new TokenValidationParameters
-                {
+            services.AddAuthentication (JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer (Options => {
+                    Options.TokenValidationParameters = new TokenValidationParameters {
                     ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
-                                        .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                    IssuerSigningKey = new SymmetricSecurityKey (Encoding.ASCII
+                    .GetBytes (Configuration.GetSection ("AppSettings:Token").Value)),
                     ValidateIssuer = false,
                     ValidateAudience = false
-                };
-            });
+                    };
+                });
 
-            services.AddScoped<LogUserActivity>();
+            services.AddScoped<LogUserActivity> ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler(builder =>
-                {
-                    builder.Run(async context =>
-                    {
-                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                        var error = context.Features.Get<IExceptionHandlerFeature>();
-                        if (error != null)
-                        {
-                            context.Response.AddApplicationError(error.Error.Message);
-                            await context.Response.WriteAsync(error.Error.Message);
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env, Seed seeder) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            } else {
+                app.UseExceptionHandler (builder => {
+                    builder.Run (async context => {
+                        context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+                        var error = context.Features.Get<IExceptionHandlerFeature> ();
+                        if (error != null) {
+                            context.Response.AddApplicationError (error.Error.Message);
+                            await context.Response.WriteAsync (error.Error.Message);
                         }
                     });
                 });
-                //app.UseHsts();
+                app.UseHsts ();
             }
 
             // app.UseHttpsRedirection();
             //seeder.SeedUsers();
-            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-            app.UseAuthentication();
-            app.UseMvc();
+            app.UseCors (x => x.AllowAnyOrigin ().AllowAnyMethod ().AllowAnyHeader ());
+            app.UseAuthentication ();
+            app.UseDefaultFiles ();
+            app.UseStaticFiles ();
+            app.UseMvc (routes => {
+                routes.MapSpaFallbackRoute (
+                    name: "spa-fallback",
+                    defaults : new {
+                        controller = "Fallback",
+                            action = "Index"
+                    }
+                );
+            });
         }
     }
 }
